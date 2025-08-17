1511d27061ea0ff753dfa203462b92c4bcb7c533
ごてんば <78265558+GotembaYuka@users.noreply.github.com>
2025-08-17 12:55:10 +0900

    kousin

--- minecraft_addons/z_addon_list.js
+++ minecraft_addons/z_addon_list.js
@@ -5,15 +5,15 @@ let adlist = {
       description: "金床で改行テキストを使用できます。",
       type: ["リソースパック"],
       junban: 1,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/Newline-Text_for_Anvil/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/Newline-Text_for_Anvil/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/Newline-Text_for_Anvil/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/Newline-Text_for_Anvil/releases/latest"
     },
     {
       name: "Custom UI Maker",
       description: "本と羽根ペンを使って内容を自由に入力して表示できるUIを作ることができます!",
       type: ["リソースパック"],
       junban: 4,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/custom-ui-maker/refs/heads/main/pack_icon.png",
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/custom-ui-maker/refs/heads/main/pack_icon.png",
       link: "./custom_ui_maker.html"
     },
     {
@@ -21,7 +21,7 @@ let adlist = {
       description: "コンソールエディション(WiiU, 旧Switch, PS3, 旧PS4, Xbox360, 旧Xbox One)のようなUIにするリソースパックです!",
       type: ["リソースパック"],
       junban: 5,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/ConsoleUI/refs/heads/main/pack_icon.png",
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/ConsoleUI/refs/heads/main/pack_icon.png",
       link: "./console_ui.html"
     },
     {
@@ -29,40 +29,40 @@ let adlist = {
       description: "カスタムボスバーをscoreboardのsidebarで作れます。<br>/scoreboard objectives add &lt;ID&gt; dummy",
       type: ["リソースパック"],
       junban: 6,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/Custom-bossbar-addon/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/Custom-bossbar-addon/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/Custom-bossbar-addon/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/Custom-bossbar-addon/releases/latest"
     },
     {
       name: "チャットでセクション装飾消す",
       description: "チャットでのセクション装飾を消します<br>全てのサーバーで動くわけではありません!",
       type: ["リソースパック"],
       junban: 7,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/anti_section_decoration_in_chat/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/anti_section_decoration_in_chat/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/anti_section_decoration_in_chat/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/anti_section_decoration_in_chat/releases/latest"
     },
     {
       name: "Preview デバッグテキスト切り替え",
       description: "Preview版の上のデバッグテキストを非表示にしたりできます。<br>HUD画面でF10を押して切り替えます。",
       type: ["リソースパック"],
       junban: 8,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/debug_text_toggle/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/debug_text_toggle/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/debug_text_toggle/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/debug_text_toggle/releases/latest"
     },
     {
       name: "グリッド表示のサーバーフォーム",
       description: "サーバーフォームをグリッド表示にします",
       type: ["リソースパック"],
       junban: 9,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/grid-sever-form/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/grid-sever-form/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/grid-sever-form/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/grid-sever-form/releases/latest"
     },
     {
       name: "Chat Text Copy",
       description: "チャットの文章を簡単にコピペできます",
       type: ["リソースパック"],
       junban: 10,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/chat-text-copy/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/chat-text-copy/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/chat-text-copy/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/chat-text-copy/releases/latest"
     },
     {
       name: "Fullscreen Play Screen",
@@ -70,7 +70,7 @@ let adlist = {
       type: ["リソースパック"],
       junban: 11,
       icon_url: "../textures/minecraft_icon.png",
-      link: "https://github.com/YuuhaLand/fullscreen-play-screen/releases/latest"
+      link: "https://github.com/GotembaYuka/fullscreen-play-screen/releases/latest"
     },
     {
       name: "SNS Feed Screen",
@@ -78,94 +78,94 @@ let adlist = {
       type: ["リソースパック"],
       junban: 12,
       icon_url: "../textures/minecraft_icon.png",
-      link: "https://github.com/YuuhaLand/Compact_Feed_Screen/releases/latest"
+      link: "https://github.com/GotembaYuka/Compact_Feed_Screen/releases/latest"
     },
     {
       name: "Fullscreen Bock Screen",
       description: "Gote-Supporterにあったフルスクリーンの本画面です。",
       type: ["リソースパック"],
       junban: 13,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/fullscreen-bock-screen/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/fullscreen-bock-screen/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/fullscreen-bock-screen/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/fullscreen-bock-screen/releases/latest"
     },
     {
       name: "タッチ操作でかんたんF5",
       description: "F5キーをスマホで押せる！<br>camera コマンドの実験を有効化する必要があります。",
       type: ["ビヘイビアーパック", "リソースパック"],
       junban: 14,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/touch-f5/refs/heads/main/resource/pack_icon.png",
-      link: "https://github.com/YuuhaLand/touch-f5/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/touch-f5/refs/heads/main/resource/pack_icon.png",
+      link: "https://github.com/GotembaYuka/touch-f5/releases/latest"
     },
     {
       name: "オンスクリーン効果",
       description: "画面効果を追加します！",
       type: ["リソースパック"],
       junban: 16,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/screen-effect/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/screen-effect/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/screen-effect/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/screen-effect/releases/latest"
     },
     {
       name: "Online Player Count",
       description: "オンラインのプレイヤー数をポーズ画面に表示します！",
       type: ["リソースパック"],
       junban: 19,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/Online-Player-Count/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/Online-Player-Count/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/Online-Player-Count/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/Online-Player-Count/releases/latest"
     },
     {
       name: "Tab Key Playerlist",
       description: "このマイクラ統合版用リソースパックは、<br>HUDでTabキーを押すと、プレイヤーリストを表示することができます。",
       type: ["リソースパック"],
       junban: 20,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/Tabkey_Playerlist_UI/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/Tabkey_Playerlist_UI/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/Tabkey_Playerlist_UI/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/Tabkey_Playerlist_UI/releases/latest"
     },
     {
       name: "水の音を昔のようにする for v1.20.30",
       description: "水のサウンドを1.20.10以前のようにします 1.20.30にのみ対応",
       type: ["リソースパック"],
       junban: 22,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/fixed-water/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/fixed-water/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/fixed-water/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/fixed-water/releases/latest"
     },
     {
       name: "Safezone filler",
       description: "より小さいGUIにできます！",
       type: ["リソースパック"],
       junban: 23,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/safezone-filler/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/safezone-filler/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/safezone-filler/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/safezone-filler/releases/latest"
     },
     {
       name: "三人称カメラの仕様を戻す",
       description: "三人称カメラで音をプレイヤー基準の仕様に戻します",
       type: ["ビヘイビアーパック"],
       junban: 24,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/camera-sound-player/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/camera-sound-player/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/camera-sound-player/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/camera-sound-player/releases/latest"
     },
     {
       name: "UIパフォーマンス改善",
       description: "UIでのメモリ使用量を抑え、パフォーマンスを改善します",
       type: ["リソースパック"],
       junban: 26,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/memori-sakugen-ui/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/memori-sakugen-ui/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/memori-sakugen-ui/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/memori-sakugen-ui/releases/latest"
     },
     {
       name: "Item ID Auxを見れるやつ",
       description: "アイテムのItem ID Auxをインベントリから見れます。<br>タッチ非対応です",
       type: ["リソースパック"],
       junban: 28,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/item-id-aux-hyouzi/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/item-id-aux-hyouzi/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/item-id-aux-hyouzi/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/item-id-aux-hyouzi/releases/latest"
     },
     {
       name: "Unicodeフォント化",
       description: "フォントをUnicodeフォント化します<br>言語 日本語がおすすめです!",
       type: ["リソースパック"],
       junban: 29,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/compulsion-unicode-font/refs/heads/main/pack_icon.png",
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/compulsion-unicode-font/refs/heads/main/pack_icon.png",
       link: "./compulsion-unicode-font.html"
     },
     {
@@ -173,7 +173,7 @@ let adlist = {
       description: "大きくなった日本語フォントを小さくする！ +文字化け改善！<br>グロリソ禁止ワールドでも機能します！",
       type: ["リソースパック"],
       junban: 30,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/small-font/refs/heads/main/pack_icon.png",
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/small-font/refs/heads/main/pack_icon.png",
       link: "./small-font.html"
     },
     {
@@ -181,31 +181,31 @@ let adlist = {
       description: "アルファ時代のスタート画面を再現しました。",
       type: ["リソースパック"],
       junban: 31,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/alpha_startscreen/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/alpha_startscreen/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/alpha_startscreen/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/alpha_startscreen/releases/latest"
     },
     {
       name: "即抜けRTA",
       description: "即抜けを検知して通知します!",
       type: ["ビヘイビアーパック"],
       junban: 32,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/Maikura_de_Sokunuke_RTA/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/Maikura_de_Sokunuke_RTA/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/Maikura_de_Sokunuke_RTA/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/Maikura_de_Sokunuke_RTA/releases/latest"
     },
     {
       name: "OreUI風 ポップアップ",
       description: "ポップアップダイアログをOreUI風にします",
       type: ["リソースパック"],
       junban: 33,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/OreUI-Popup/refs/heads/main/pack_icon.png",
-      link: "https://github.com/YuuhaLand/OreUI-Popup/releases/latest"
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/OreUI-Popup/refs/heads/main/pack_icon.png",
+      link: "https://github.com/GotembaYuka/OreUI-Popup/releases/latest"
     },
     {
       name: "HUDチャット アニメーション",
       description: "HUDのチャットにアニメーションを追加します！",
       type: ["リソースパック"],
       junban: 34,
-      icon_url: "https://raw.githubusercontent.com/YuuhaLand/BetterChatAnimation/refs/heads/main/pack_icon.png",
+      icon_url: "https://raw.githubusercontent.com/GotembaYuka/BetterChatAnimation/refs/heads/main/pack_icon.png",
       link: "./chat_animation.html"
     },
   ]
